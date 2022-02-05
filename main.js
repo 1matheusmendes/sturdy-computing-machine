73a7caaca730592000476f22225b56ec4a455b07
1matheusmendes <matheus_mendess@live.com>
2022-02-05 13:27:11 -0300

    funçao verifica idade ok

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+//Matheus Mendes
+
+function verificaData(){
+
+    var dataAtual = Date();
+
+    console.log(dataAtual)
+
+    var dataInformada = document.getElementById("data").value;
+
+
+    console.log(dataInformada);
+
+}
+
+
+function idadeInformada(){
+
+    var idade = parseInt (document.getElementById("idade").value);
+    
+    if (idade > 18){
+        console.log("Ok")
+    }
+    else
+        alert ("Você não tem a idade minima para participar do evento");
+}
 
 function adicionar() {
     var participante = document.getElementById("participantes").value;
